37ae8915c64ab67f3182c89e70009aa6243f8662
Parker Manning <parkerlmanning@hotmail.com>
2021-01-04 21:13:08 -0600

    fixed some of the server page, testing heroku

--- server.js
+++ server.js
@@ -1,41 +1,27 @@
-
 const express = require("express");
-
 const logger = require("morgan");
-
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
-// call express server
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-mongoose.connect(
-process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker123456',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-
-// Call Express Routes
+// routes
 app.use(require("./routes/api.js"));
-
 app.use(require("./routes/view.js"));
 
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
